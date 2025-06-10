3b8011cd79d7f32a92d08e8f385a4c5fc9e12171
Haecheol Shin <dev.hc.shin@gmail.com>
2025-06-10 15:30:16 +0900

    first commit

---
+++ src/test/java/com/example/LLMService/LlmServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LLMService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
